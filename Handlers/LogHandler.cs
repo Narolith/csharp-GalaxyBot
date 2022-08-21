f47ad7e001a6b9d936255cc38396b3136b14010a
Stephen Cherry <stephen.m.cherry91@gmail.com>
2022-08-21 16:03:17 -0400

    Added Log channel

--- Handlers/LogHandler.cs
+++ Handlers/LogHandler.cs
@@ -1,4 +1,6 @@
 using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace GalaxyBot.Handlers
 {
@@ -7,8 +9,10 @@ namespace GalaxyBot.Handlers
     /// </summary>
     public class LogHandler
     {
-        public static Task LogAsync(LogMessage log)
+        public static Task LogConsoleAsync(LogMessage log)
         {
+            var logMsg = $"{DateTime.Now,-19} [{log.Severity,8}] {log.Source}: {log.Message} {log.Exception}";
+
             var cc = Console.ForegroundColor;
 
             switch (log.Severity)
@@ -32,10 +36,45 @@ namespace GalaxyBot.Handlers
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                     break;
             }
-
-            Console.WriteLine($"{DateTime.Now,-19} [{log.Severity,8}] {log.Source}: {log.Message} {log.Exception}");
-
+            Console.WriteLine(logMsg);
             Console.ForegroundColor = cc;
+
+            return Task.CompletedTask;
+        }
+
+        public static Task LogChannelAsync(SocketTextChannel logChannel, LogMessage log)
+        {
+            Task.Run(async () =>
+            {
+                var embed = new EmbedBuilder()
+                    .WithColor(new Color(0xFF0000))
+                    .WithTitle($"{log.Source} [{log.Severity}]")
+                    .WithDescription($"{ log.Message}\n{ log.Exception}")
+                    .WithTimestamp(DateTime.UtcNow);
+                
+                switch (log.Severity)
+                {
+                    case LogSeverity.Critical:
+                        embed.WithColor(new Color(0xFF0000));
+                        break;
+                    case LogSeverity.Error:
+                        embed.WithColor(new Color(0xFF0000));
+                        break;
+                    case LogSeverity.Warning:
+                        embed.WithColor(new Color(0xFFA500));
+                        break;
+                    case LogSeverity.Info:
+                        embed.WithColor(new Color(0xFFFFFF));
+                        break;
+                    case LogSeverity.Verbose:
+                        embed.WithColor(new Color(0xC0C0C0));
+                        break;
+                    case LogSeverity.Debug:
+                        embed.WithColor(new Color(0x808080));
+                        break;
+                }
+                await logChannel.SendMessageAsync(embed: embed.Build());
+            });
 
             return Task.CompletedTask;
         }
